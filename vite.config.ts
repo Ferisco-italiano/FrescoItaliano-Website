f7046dc55bac78eaa30fbdfa0f4551d804ddb1f1
Sanfutty14 <sanfar@psts-sg.com>
2026-08-14 15:37:22 +0530

    github config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: mode === 'production' ? '/FrescoItaliano-Website/' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
